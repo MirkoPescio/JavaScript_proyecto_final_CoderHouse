a038709c421d03a8e5760ea0e02066e0e1302be5
Mirko Pescio <mirkopes.4050@gmail.com>
2021-12-26 17:45:11 -0300

    Aplicando el carrito de compras al resto de las secciones

--- js/scripts.js
+++ js/scripts.js
@@ -30,33 +30,48 @@ Defino objetos y listas que pueda usar más adelante
 // Agrego 2 productos nuevos:
 
 class producto {
-    constructor(imagen, nombre, precio) {
+    constructor(imagen, nombre, precio, dataID) {
         this.imagen = imagen;
         this.nombre = nombre;
         this.precio = precio;
+        this.dataID = dataID;
     }
 }
 
 let arrayNuevosPopulares = [];
-arrayNuevosPopulares.push(new producto("../imagenes/batman-return-to-arkham-ps4_2.jpg", "Batman: Return to Arkham PS4", "850,00"));
-arrayNuevosPopulares.push(new producto("../imagenes/heavy-rain-ps4.jpg", "Heavy Rain PS4", "$650,00"));
+arrayNuevosPopulares.push(new producto("../imagenes/batman-return-to-arkham-ps4_2.jpg", "Batman: Return to Arkham PS4", "850,00", "batmanReturn"));
+arrayNuevosPopulares.push(new producto("../imagenes/heavy-rain-ps4.jpg", "Heavy Rain PS4", "$650,00", "heavyRain"));
 
 for (const item of arrayNuevosPopulares) {
     let principal = document.getElementById("contenedorPopulares");
     let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<div class="col mb-5">
+    contenedor.innerHTML = `
+                            <div class="col mb-5 producto">
                                 <div class="card h-100">
-                                    <img class="card-img-top" src=${item.imagen} alt="..." />
+                                    <!-- Product image-->
+                                    <img class="card-img-top" src="${item.imagen}" alt="..." />
+                                    <!-- Product details-->
                                     <div class="card-body p-4">
                                         <div class="text-center">
+                                            <!-- Product name-->
                                             <h5 class="fw-bolder">${item.nombre}</h5>
+                                            <!-- Product reviews-->
+                                            <div class="d-flex justify-content-center small text-warning mb-2">
+                                                <div class="bi-star-fill"></div>
+                                                <div class="bi-star-fill"></div>
+                                                <div class="bi-star-fill"></div>
+                                                <div class="bi-star-fill"></div>
+                                                <div class="bi-star-fill"></div>
+                                            </div>
                                             <!-- Product price-->
-                                            ${item.precio}
+                                            <div class="contenedorPrecio">
+                                                ${item.precio}
+                                            </div>
                                         </div>
                                     </div>
                                     <!-- Product actions-->
                                     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir al carrito</a></div>
+                                        <div class="text-center"><button type="submit" class="btn btn-outline-dark mt-auto agregar-carrito" data-id="${item.dataID}">Añadir al carrito</button></div>
                                     </div>
                                 </div>
                             </div>`;
